0e519f906c0e00092b8b2c39ae7474555d7d1cf2
Rodrigo Gonzalez Jorquera <rgonzalezj@gmail.com>
2020-09-08 22:54:55 -0300

    commit inicial

---
+++ buid_web_page.rb
@@ -0,0 +1,22 @@
+
+=begin
+1. Crear el método request que reciba una url y el api_key y devuelva el hash con los resultados
+Concatenar la API Key en la url
+2. Crear un método llamado buid_web_page que reciba el hash de respuesta con todos los datos
+y construya una página web. Se evaluará la página creada y tiene que tener este formato:     
+=end
+
+
+buid_web_page
+
+
+<html>
+<head>
+</head>
+<body>
+<ul>
+<li><img src='.../398380645PRCLF0030000CCAM04010L1.PNG'></li>
+<li><img src='.../398381687EDR_F0030000CCAM05010M_.JPG'></li>
+</ul>
+</body>
+</html>
