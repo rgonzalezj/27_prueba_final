f0b709ca8ec4e4cf8a99083a25fa5e6c7ae65f07
Rodrigo Gonzalez Jorquera <rgonzalezj@gmail.com>
2020-09-10 22:23:04 -0300

    Implementacion codigo NASA con ID Rodrigo Gonzlez Jorquera

--- buid_web_page.rb
+++ buid_web_page.rb
@@ -1,4 +1,3 @@
-
 =begin
 1. Crear el método request que reciba una url y el api_key y devuelva el hash con los resultados
 Concatenar la API Key en la url
@@ -6,15 +5,16 @@ Concatenar la API Key en la url
 y construya una página web. Se evaluará la página creada y tiene que tener este formato:     
 =end
 
+#NASA ID Rodrigo González Jorquera = K2rMhlaiuTIP0drR6ZkZ0HHCp6O7QgGwIVvCB7bf
+
 require "uri"
 require "net/http"
+require "json"
 
-url = URI("http://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=DEMO_KEY ")
+url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=K2rMhlaiuTIP0drR6ZkZ0HHCp6O7QgGwIVvCB7bf")
 
 https = Net::HTTP.new(url.host, url.port);
 https.use_ssl = true
-
 request = Net::HTTP::Get.new(url)
-
 response = https.request(request)
-puts response.read_body
+puts JSON.parse(response.read_body)
