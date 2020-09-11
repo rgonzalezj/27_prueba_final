361f89255f9de33caa44652e62cd3a54acc52ab2
Rodrigo Gonzalez Jorquera <rgonzalezj@gmail.com>
2020-09-11 17:56:55 -0300

    Definicion de pagina web fotos-nasa.html

--- buid_web_page.rb
+++ buid_web_page.rb
@@ -8,13 +8,66 @@ y construya una página web. Se evaluará la página creada y tiene que tener es
 #NASA ID Rodrigo González Jorquera = K2rMhlaiuTIP0drR6ZkZ0HHCp6O7QgGwIVvCB7bf
 
 require "uri"
+require "openssl"
 require "net/http"
 require "json"
 
-url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=K2rMhlaiuTIP0drR6ZkZ0HHCp6O7QgGwIVvCB7bf")
+def request(urlweb, api_key)
+url = URI("#{urlweb}api_key=#{api_key}") 
 
 https = Net::HTTP.new(url.host, url.port);
-https.use_ssl = true
 request = Net::HTTP::Get.new(url)
 response = https.request(request)
-puts JSON.parse(response.read_body)
+https.use_ssl = true
+info = response.read_body
+info = JSON.parse(info)
+
+
+
+url_nasa= "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000"
+#url https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=K2rMhlaiuTIP0drR6ZkZ0HHCp6O7QgGwIVvCB7bf
+api_key= "K2rMhlaiuTIP0drR6ZkZ0HHCp6O7QgGwIVvCB7bf"
+
+nasa_pics = request(url_nasa, api_key)
+
+output_nasa = {}
+i = 0
+url_nasa["photos"].each do |hash|
+    hash.each do |key, value|
+        if key == "img_src"
+            url_nasa["key#{i}"] = value
+            i += 1
+        end
+    end
+    print output_nasa
+end
+end
+
+def build_web_page(output_nasa)
+    head = '<title>Fotos NASA</title>
+	<meta name="description" content="Fotos de la NASA para nave Curiosity,ordenadas por li para la prueba dee DLG34"/>
+	<meta name="keywords" content="nasa, curiosity, marte, fotos de la nasa"/>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'
+    
+    body = "<ul></ul>"
+    footer = '<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+  </html>'
+
+    web = (head+body+footer)
+    
+    output_nasa.each do |key, value|
+        value.each do |i|
+            body += "<li><img src=\"#{i}\">...</img></li>"
+        end
+    end
+    return web
+    File.write('fotos-nasa.html', html)
+end
